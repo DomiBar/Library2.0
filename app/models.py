91fccf6342746895d6e4c46f23a78f1e39abac19
Dominik Baran <dominik.baran1992@gmail.com>
2020-08-23 20:49:29 +0200

    Update databases

--- app/models.py
+++ app/models.py
@@ -1,39 +1,43 @@
 from app import db
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
 Base = declarative_base()
 
-association_table = Table('association', Base.metadata,
-                          Column('author_id', Integer,
-                                 ForeignKey('author.id')),
-                          Column('book_id', Integer, ForeignKey('book.id'))
-                          )
+
+association_table = db.Table('association_table',
+                             db.Column('author_id', db.Integer,
+                                       db.ForeignKey('author.id')),
+                             db.Column('book_id', db.Integer,
+                                       db.ForeignKey('book.id'))
+                             )
 
 
 class Author(db.Model):
-    __tablename__ = 'author'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), index=True)
     last_name = db.Column(db.String(50), index=True)
-    books = db.relationship("book", secondary=association_table,
-                            backref="author", lazy="dynamic")
+    books = db.relationship("Book", secondary=association_table,
+                            backref=db.backref("author", lazy='dynamic'))
 
     def __str__(self):
         return f"<Author {self.first_name} {self.last_name}>"
 
 
+class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.relationship("Book", backref="state", lazy='dynamic')
+    avaliable = db.Column(db.Boolean)
+
+
 class Book(db.Model):
-    __tablename__ = 'book'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), index=True)
-    authors = db.relationship("author", secondary=association_table,
-                              backref="book", lazy="dynamic")
+    authors = db.relationship("Author", secondary=association_table,
+                              backref=db.backref("book", lazy='dynamic'))
     description = db.Column(db.Text)
     year = db.Column(db.Integer)
-    cover = db.Column(db.String(200))
+    avaliable = db.Column(db.Integer, db.ForeignKey('state.id'))
 
     def __str__(self):
         return f"<Post {self.id} {self.title}>"
--- library.py
+++ library.py
@@ -1,10 +1,11 @@
 from app import app, db
-from app.models import Author, Book
+from app.models import Author, Book, State
 
 @app.shell_context_processor
 def make_shell_context():
    return {
        "db": db,
        "Author": Author,
-       "Book": Book
+       "Book": Book,
+       "State": State
    }
